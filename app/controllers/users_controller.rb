c013d93bcb227c00e347fd6dda91e51ef0b1d963
minhthinh1612 <minhthinh1995@gmail.com>
2019-03-15 20:47:37 +0700

    Basic-login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,10 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+private
 
-    def user_params
-      params.require(:user).permit :user_name, :email, :password,
-                                   :password_confirmation
-    end
+  def user_params
+    params.require(:user).permit :user_name, :email, :password,
+                                 :password_confirmation
+  end
 end
--- app/helpers/static_pages_helper.rb
+++
@@ -1,6 +0,0 @@
-module StaticPagesHelper
-  def full_title(page_title = "")
-    base_title = "WELearn - Học online mọi lúc"
-    page_title.empty? ? (base_title) : (page_title + " - " + base_title)
-  end
-end
