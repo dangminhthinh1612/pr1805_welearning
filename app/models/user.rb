d56c22465a366e1c0934c1b9024ad0243b18ca2c
minhthinh1612 <minhthinh1995@gmail.com>
2019-03-17 16:48:03 +0700

    Advanced-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :user_courses, class_name: UserCourse.name
   has_many :courses, through: :user_courses
 
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :user_name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -18,5 +19,23 @@ class User < ApplicationRecord
                                                     BCrypt::Engine.cost
       BCrypt::Password.create string, cost: cost
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new remember_digest.is_password? remember_token
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
   end
 end
