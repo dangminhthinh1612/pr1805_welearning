54c8384e11ca0faa289eb8e94d4365742728dd7d
Ngoc <huungoc2705@gmail.com>
2019-03-18 18:19:10 +0700

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
   get "/news", to: "static_pages#news"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get  "/signup",  to: "users#new"
+  post "/signup",  to: "users#create"
+  root "users#new"
+  resources :users
 end
